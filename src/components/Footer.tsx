130d05388fa07456716ad946328fc309c8e4d4bf
mandfredleow <44769311+mandfredleow@users.noreply.github.com>
2024-09-09 17:38:24 +0800

    Update warnings regarding Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,24 +6,24 @@ const Footer = () => {
           <div>
             <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">Company</h3>
             <ul className="mt-4 space-y-4">
-              <li><a className="text-base text-gray-500 hover:text-gray-900">About</a></li>
-              <li><a className="text-base text-gray-500 hover:text-gray-900">Blog</a></li>
-              <li><a className="text-base text-gray-500 hover:text-gray-900">Careers</a></li>
+              <li><a href="#" className="text-base text-gray-500 hover:text-gray-900">About</a></li>
+              <li><a href="#" className="text-base text-gray-500 hover:text-gray-900">Blog</a></li>
+              <li><a href="#" className="text-base text-gray-500 hover:text-gray-900">Careers</a></li>
             </ul>
           </div>
           <div>
             <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">Support</h3>
             <ul className="mt-4 space-y-4">
-              <li><a className="text-base text-gray-500 hover:text-gray-900">Contact</a></li>
-              <li><a className="text-base text-gray-500 hover:text-gray-900">Shipping</a></li>
-              <li><a className="text-base text-gray-500 hover:text-gray-900">Returns</a></li>
+              <li><a href="#" className="text-base text-gray-500 hover:text-gray-900">Contact</a></li>
+              <li><a href="#" className="text-base text-gray-500 hover:text-gray-900">Shipping</a></li>
+              <li><a href="#" className="text-base text-gray-500 hover:text-gray-900">Returns</a></li>
             </ul>
           </div>
           <div>
             <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">Legal</h3>
             <ul className="mt-4 space-y-4">
-              <li><a className="text-base text-gray-500 hover:text-gray-900">Privacy</a></li>
-              <li><a className="text-base text-gray-500 hover:text-gray-900">Terms</a></li>
+              <li><a href="#" className="text-base text-gray-500 hover:text-gray-900">Privacy</a></li>
+              <li><a href="#" className="text-base text-gray-500 hover:text-gray-900">Terms</a></li>
             </ul>
           </div>
         </div>
